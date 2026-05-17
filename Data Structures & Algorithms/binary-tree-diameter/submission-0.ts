94a8b4e90bf811beaefe8b2cfd1f672389b54430
Artur Moreira <63562936+arturacm@users.noreply.github.com>
2026-05-17 14:30:43 -0400

    Add: binary-tree-diameter - submission-0

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-0.ts
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    diameterOfBinaryTree(root: TreeNode | null): number {
+        let diameter = 0;
+
+        function recursive(root: TreeNode | null): number {
+            if (root === null) return 0;
+
+            const left = recursive(root.left);
+            const right = recursive(root.right);
+
+            diameter = Math.max(diameter, left + right);
+            return Math.max(left,right) + 1;
+        }
+        recursive(root);
+
+        return diameter;
+    }
+}
