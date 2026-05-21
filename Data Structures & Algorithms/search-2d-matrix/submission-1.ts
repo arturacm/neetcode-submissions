afab5ff57b3bbd8932c1a04ba75a75460bae44c6
Artur Moreira <63562936+arturacm@users.noreply.github.com>
2026-05-21 11:29:58 -0400

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.ts
@@ -0,0 +1,59 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix: number[][], target: number): boolean {
+        if (matrix.length == 0) return false;
+        // search in which row it is
+
+        let left = 0;
+        let right = matrix.length -1;
+
+        let row = null;
+        while (left <= right){
+            console.log("row", left, right)
+            if (right === matrix.length -1 && target > matrix[right][0]){
+                row  = right;
+                break
+            }
+            let mid = Math.floor((right + left)/2);
+
+            if (matrix[mid][0] === target){
+                return true;
+            } 
+
+            if (matrix[mid][0] <= target && matrix[mid][matrix[mid].length - 1] >= target){
+                row = mid;
+                break;
+            }
+
+            if (matrix[mid][0] > target){
+                right = mid - 1;
+            } else {
+                left = mid + 1
+            }
+        }
+
+        if (row === null) return false;
+        
+        left = 0;
+        right = matrix[row].length -1;
+
+        while (left <= right){
+            let mid = Math.floor((right + left)/2);
+
+            if (matrix[row][mid] === target){
+                return true;
+            } 
+
+            if (matrix[row][mid] > target){
+                right = mid - 1;
+            } else {
+                left = mid + 1
+            }
+        }
+        return false
+    }
+}
