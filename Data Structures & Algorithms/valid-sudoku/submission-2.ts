ac1a34be75cc08ffddf94274a821fded8fe3b3e8
Artur Moreira <63562936+arturacm@users.noreply.github.com>
2026-06-16 10:59:51 -0400

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.ts
@@ -0,0 +1,48 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {boolean}
+     */
+    isValidSudoku(board: string[][]): boolean {
+        const columns = new Array(board[0].length).fill(null).map(() => new Set());
+        const lines = new Array(board[0].length).fill(null).map(() => new Set());
+        const squares = new Map<string, Set<string>>();
+
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
+                let num = board[i][j];
+                if (num == ".") continue;
+
+                const seenLin = lines[i];
+                if (seenLin.has(num)) {
+                    return false;
+                } else {
+                    seenLin.add(num);
+                }
+
+                const seenCol = columns[j];
+                if (seenCol.has(num)) {
+                    return false;
+                } else {
+                    seenCol.add(num);
+                }
+
+                const squaresKey = `${Math.floor(i / 3)}:${Math.floor(j / 3)}`;
+                let seenSquares;
+                if (squares.has(squaresKey)) {
+                    seenSquares = squares.get(squaresKey);
+                } else {
+                    seenSquares = new Set<string>();
+                    squares.set(squaresKey, seenSquares);
+                }
+                if (seenSquares.has(num)) {
+                    return false;
+                } else {
+                    seenSquares.add(num);
+                }
+            }
+        }
+        return true;
+    }
+}
+
