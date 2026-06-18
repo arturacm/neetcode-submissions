6c65b20bb3a5d00f8d24ea6f4d7857d6a8871cda
Artur Moreira <63562936+arturacm@users.noreply.github.com>
2026-06-18 15:52:43 -0400

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices: number[]): number {
+
+        let maxProfit = 0;
+        let left = 0;
+        let right = 1;
+
+        while (left< right && right < prices.length){
+            let buy = prices[left];
+            let sell = prices[right];
+
+            if (sell <= buy){
+                left = right;
+                right++;
+            } else {
+                maxProfit = Math.max(maxProfit, sell-buy)
+                right++;
+            }
+        }
+        return maxProfit;
+    }
+}
